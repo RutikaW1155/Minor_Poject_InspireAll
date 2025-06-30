3dea0201268c497b02adf0f023a0f3d539a14400
Gayatri  Dandgal <131341455+Gayatridandgal@users.noreply.github.com>
2025-06-30 22:13:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
 import HowItWorks from './components/HowItWorks';
 import CoreFeatures from './components/CoreFeatures';
